d0b87b8bd9903a3f124bedf3408b7753ba6fee47
BM85-Hz <53278075+BM85-Hz@users.noreply.github.com>
2023-10-17 18:35:56 -0700

    added auto log generation for first time use
    -also circumvents the filewatcher not seeing the log update since it exists from the get-go

--- ROMRecordQt/main.cpp
+++ ROMRecordQt/main.cpp
@@ -6,6 +6,7 @@
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 #include <QCoreApplication>
+#include <QFile>
 #include <QtCore>
 #include <QtNetwork/QNetworkAccessManager>
 #include <QtNetwork/QNetworkRequest>
@@ -15,6 +16,24 @@
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
+
+    QVarLengthArray<QString> fileList;
+    fileList.append("calendarlog.json");
+    fileList.append("recentslog.json");
+    fileList.append("highestlog.json");
+
+    // Generates log files on first use or after deletion
+    for(auto& fileName : fileList){
+        if (!QFile::exists(fileName)){
+            QFile file(fileName);
+
+            if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+                file.close();
+            } else {
+                qWarning("Could not create the file.");
+            }
+        }
+    }
 
     MainWindow window;
     window.show();
